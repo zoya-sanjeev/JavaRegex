2752649a61a048ec8cabf9237e27c45c74da535e
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-07 17:51:37 +0530

    ADDED: method to validate name

--- src/com/bridgelabz/userregistration/UserRegistration.java
+++ src/com/bridgelabz/userregistration/UserRegistration.java
@@ -1,9 +1,22 @@
 package com.bridgelabz.userregistration;
 
+import java.util.Scanner;
+import java.util.regex.*;
 public class UserRegistration {
+	
+	public static boolean nameValidation(String name) {
+		String nameValidation="^[A-Z][a-z]{2,}$";
+		return Pattern.matches(nameValidation, name);
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner=new Scanner(System.in);
+		
+		System.out.println("Enter name");
+		String name=scanner.next();
+		if(!nameValidation(name))
+			System.out.println("Invalid format");
+		
 
 	}
 
